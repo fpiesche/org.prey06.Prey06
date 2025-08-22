5ca51c0de33e8063831569cd3f5af4046bf248b2
Krispy <nikodash2020@hotmail.com>
2025-08-21 21:43:10 -0500

    Initial Commit

--- test-build.sh
+++ test-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 clear
-flatpak-builder --repo=testing-repo --force-clean build-dir org.dhewm3.Dhewm3.yaml
-flatpak --user remote-add --if-not-exists --no-gpg-verify dhewm3-testing-repo testing-repo
-flatpak --user install dhewm3-testing-repo org.dhewm3.Dhewm3 -y
-flatpak --user install dhewm3-testing-repo org.dhewm3.Dhewm3.Debug -y
+flatpak-builder --repo=testing-repo --force-clean build-dir org.prey06.Prey06.yaml
+flatpak --user remote-add --if-not-exists --no-gpg-verify prey06-testing-repo testing-repo
+flatpak --user install prey06-testing-repo org.prey06.Prey06 -y
+flatpak --user install prey06-testing-repo org.prey06.Prey06.Debug -y
 flatpak update -y
